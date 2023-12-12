14e05d7d6962783a049f4d7a76e32bc60c95891a
YassinEldeeb <yassineldeeb94@gmail.com>
2023-12-12 06:41:52 +0200

    fix readme

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 > [!IMPORTANT]
-        #### Remember to give github actions write access through your repostiory settings, it only has read permissions by default.
+#### Remember to give github actions write access through your repostiory settings, it only has read permissions by default.
 
-
-        # [Something] Benchmarks 
+# [Something] Benchmarks 
 
 > Note: benchmarks are ran within GitHub CI which might introduce a bit of noise, though, we can make a bold assumption that noise that will affect one will affect others. Since the benchmark only lasts a few seconds each.
         
@@ -23,6 +22,20 @@ running 0 tests
 
 test result: ok. 0 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
 
+Something1              time:   [376.11 ps 376.35 ps 376.60 ps]
+Found 4 outliers among 100 measurements (4.00%)
+  3 (3.00%) high mild
+  1 (1.00%) high severe
+
+Something2              time:   [375.09 ps 375.24 ps 375.40 ps]
+Found 4 outliers among 100 measurements (4.00%)
+  1 (1.00%) high mild
+  3 (3.00%) high severe
+
+Something3              time:   [376.02 ps 376.38 ps 376.81 ps]
+Found 11 outliers among 100 measurements (11.00%)
+  4 (4.00%) high mild
+  7 (7.00%) high severe
 
 
 ```
--- benches/something.rs
+++ benches/something.rs
@@ -2,18 +2,30 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 pub fn something(c: &mut Criterion) {
     c.bench_function("Something1", |b| {
-        // Something1 usage
-        // wrap value `black_box` to disable/avoid compiler optimizations
+        b.iter(|| {
+            // Something1 usage
+            // wrap value `black_box` to disable/avoid compiler optimizations
+            let x = i32::pow(2, 200);
+            black_box(x);
+        })
     });
 
     c.bench_function("Something2", |b| {
-        // Something2 usage
-        // wrap value `black_box` to disable/avoid compiler optimizations
+        b.iter(|| {
+            // Something2 usage
+            // wrap value `black_box` to disable/avoid compiler optimizations
+            let x = i32::ilog(2, 40);
+            black_box(x);
+        })
     });
 
     c.bench_function("Something3", |b| {
-        // Something3 usage
-        // wrap value `black_box` to disable/avoid compiler optimizations
+        b.iter(|| {
+            // Something3 usage
+            // wrap value `black_box` to disable/avoid compiler optimizations
+            let x = i32::ilog(9, 100);
+            black_box(x);
+        })
     });
 }
 
